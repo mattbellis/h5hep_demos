8cdc0261e2112315bb9628e27018f7602db18c36
mattbellis <matthew.bellis@gmail.com>
2018-04-01 04:16:50 -0400

    Stuff.

---
+++ write_narrow_HEP_file_to_ROOT.py
@@ -0,0 +1,134 @@
+import ROOT 
+from array import array
+import numpy as np
+from numpy.random import beta
+
+import sys
+
+
+################################################################################
+def calc_energy(mass,px,py,pz):
+
+    energy = np.sqrt(mass*mass + px*px + py*py + pz*pz)
+
+    return energy
+
+################################################################################
+
+nevents = 1000
+if len(sys.argv)>1:
+    nevents = int(sys.argv[1])
+
+# Create the file and "cd" into it.
+name = 'HEP_narrow_file_ROOT_n%d.root' % (nevents)
+f = ROOT.TFile(name, "RECREATE")
+f.cd()
+
+
+print("HERE")
+
+# Make the tree
+tree = ROOT.TTree( 'T', 'My tree' )
+
+njet = array('i', [-1]); tree.Branch('njet', njet, 'njet/I')
+jete = array('f', 16*[-1.]); tree.Branch('jete', jete, 'jete[njet]/F')
+jetpx = array('f', 16*[-1.]); tree.Branch('jetpx', jetpx, 'jetpx[njet]/F')
+jetpy = array('f', 16*[-1.]); tree.Branch('jetpy', jetpy, 'jetpy[njet]/F')
+jetpz = array('f', 16*[-1.]); tree.Branch('jetpz', jetpz, 'jetpz[njet]/F')
+jetbtag = array('f', 16*[-1.]); tree.Branch('jetbtag', jetbtag, 'jetbtag[njet]/F')
+
+
+nmuon = array('i', [-1]); tree.Branch('nmuon', nmuon, 'nmuon/I')
+muone = array('f', 16*[-1.]); tree.Branch('muone', muone, 'muone[nmuon]/F')
+muonpx = array('f', 16*[-1.]); tree.Branch('muonpx', muonpx, 'muonpx[nmuon]/F')
+muonpy = array('f', 16*[-1.]); tree.Branch('muonpy', muonpy, 'muonpy[nmuon]/F')
+muonpz = array('f', 16*[-1.]); tree.Branch('muonpz', muonpz, 'muonpz[nmuon]/F')
+muonq = array('f', 16*[-1.]); tree.Branch('muonq', muonq, 'muonq[nmuon]/F')
+
+nelectron = array('i', [-1]); tree.Branch('nelectron', nelectron, 'nelectron/I')
+electrone = array('f', 16*[-1.]); tree.Branch('electrone', electrone, 'electrone[nelectron]/F')
+electronpx = array('f', 16*[-1.]); tree.Branch('electronpx', electronpx, 'electronpx[nelectron]/F')
+electronpy = array('f', 16*[-1.]); tree.Branch('electronpy', electronpy, 'electronpy[nelectron]/F')
+electronpz = array('f', 16*[-1.]); tree.Branch('electronpz', electronpz, 'electronpz[nelectron]/F')
+electronq = array('f', 16*[-1.]); tree.Branch('electronq', electronq, 'electronq[nelectron]/F')
+
+nphoton = array('i', [-1]); tree.Branch('nphoton', nphoton, 'nphoton/I')
+photone = array('f', 16*[-1.]); tree.Branch('photone', photone, 'photone[nphoton]/F')
+photonpx = array('f', 16*[-1.]); tree.Branch('photonpx', photonpx, 'photonpx[nphoton]/F')
+photonpy = array('f', 16*[-1.]); tree.Branch('photonpy', photonpy, 'photonpy[nphoton]/F')
+photonpz = array('f', 16*[-1.]); tree.Branch('photonpz', photonpz, 'photonpz[nphoton]/F')
+
+nMET = array('i', [-1]); tree.Branch('nMET', nMET, 'nMET/I')
+METpt = array('f', 16*[-1.]); tree.Branch('METpt', METpt, 'METpt[nMET]/F')
+METphi = array('f', 16*[-1.]); tree.Branch('METphi', METphi, 'METphi[nMET]/F')
+
+'''
+hp.create_group(data,'muon',counter='nmuon')
+hp.create_dataset(data,['e','px','py','pz','q'],group='muon',dtype=float)
+
+hp.create_group(data,'electron',counter='nelectron')
+hp.create_dataset(data,['e','px','py','pz','q'],group='electron',dtype=float)
+
+hp.create_group(data,'photon',counter='nphoton')
+hp.create_dataset(data,['e','px','py','pz'],group='photon',dtype=float)
+
+hp.create_group(data,'MET',counter='nMET')
+hp.create_dataset(data,['pt','phi'],group='MET',dtype=float)
+
+'''
+
+nevents = 1000
+if len(sys.argv)>1:
+    nevents = int(sys.argv[1])
+
+
+for i in range(0,nevents):
+
+    if i%1000==0:
+        print(i)
+
+
+    njet[0] = np.random.randint(15)
+    for n in range(njet[0]):
+        jetpx[n] = 300*beta(2,9)
+        jetpy[n] = 300*beta(2,9)
+        jetpz[n] = 300*beta(2,9)
+        mass = 5*beta(2,9)
+        jete[n] = calc_energy(mass,jetpx[n],jetpy[n],jetpz[n])
+        jetbtag[n] = np.random.random()
+
+    nmuon[0] = np.random.randint(15)
+    for n in range(nmuon[0]):
+        muonpx[n] = 300*beta(2,9)
+        muonpy[n] = 300*beta(2,9)
+        muonpz[n] = 300*beta(2,9)
+        mass = 5*beta(2,9)
+        muone[n] = calc_energy(mass,muonpx[n],muonpy[n],muonpz[n])
+        muonq[n] = 2*np.random.randint(2) - 1
+
+    nelectron[0] = np.random.randint(15)
+    for n in range(nelectron[0]):
+        electronpx[n] = 300*beta(2,9)
+        electronpy[n] = 300*beta(2,9)
+        electronpz[n] = 300*beta(2,9)
+        mass = 5*beta(2,9)
+        electrone[n] = calc_energy(mass,electronpx[n],electronpy[n],electronpz[n])
+        electronq[n] = 2*np.random.randint(2) - 1
+
+    nphoton[0] = np.random.randint(15)
+    for n in range(nphoton[0]):
+        photonpx[n] = 300*beta(2,9)
+        photonpy[n] = 300*beta(2,9)
+        photonpz[n] = 300*beta(2,9)
+        mass = 5*beta(2,9)
+        photone[n] = calc_energy(mass,photonpx[n],photonpy[n],photonpz[n])
+
+    tree.Fill()
+
+# Go back "into" the file and close and write it.
+print("Writing the file...")
+f.cd()
+f.Write()
+f.Close()
+
+
---
+++ write_narrow_HEP_file_to_h5hep.py
@@ -0,0 +1,113 @@
+import numpy as np
+from numpy.random import beta
+import sys
+#sys.path.append('../h5hep')
+#from write import *
+import h5hep as hp
+
+################################################################################
+def calc_energy(mass,px,py,pz):
+
+    energy = np.sqrt(mass*mass + px*px + py*py + pz*pz)
+
+    return energy
+
+################################################################################
+
+data = hp.initialize()
+
+hp.create_group(data,'jet',counter='njet')
+hp.create_dataset(data,['e','px','py','pz','btag'],group='jet',dtype=float)
+
+hp.create_group(data,'muon',counter='nmuon')
+hp.create_dataset(data,['e','px','py','pz','q'],group='muon',dtype=float)
+
+hp.create_group(data,'electron',counter='nelectron')
+hp.create_dataset(data,['e','px','py','pz','q'],group='electron',dtype=float)
+
+hp.create_group(data,'photon',counter='nphoton')
+hp.create_dataset(data,['e','px','py','pz'],group='photon',dtype=float)
+
+hp.create_group(data,'MET',counter='nMET')
+hp.create_dataset(data,['pt','phi'],group='MET',dtype=float)
+
+event = hp.create_single_event(data)
+
+nevents = 1000
+if len(sys.argv)>1:
+    nevents = int(sys.argv[1])
+
+#print(data)
+#print(event)
+
+#'''
+for i in range(0,nevents):
+
+    if i%1000==0:
+        print(i)
+
+    hp.clear_event(event)
+
+    njet = np.random.randint(16)
+    event['jet/njet'] = njet
+    for n in range(njet):
+        px = 300*beta(2,9)
+        py = 300*beta(2,9)
+        pz = 300*beta(2,9)
+        mass = 5*beta(2,9)
+        energy = calc_energy(mass,px,py,pz)
+        event['jet/px'].append(px)
+        event['jet/py'].append(py)
+        event['jet/pz'].append(pz)
+        event['jet/e'].append(energy)
+        event['jet/btag'].append(np.random.random())
+
+    nmuon = np.random.randint(16)
+    event['muon/nmuon'] = nmuon
+    for n in range(nmuon):
+        px = 300*beta(2,9)
+        py = 300*beta(2,9)
+        pz = 300*beta(2,9)
+        mass = 0.105
+        energy = calc_energy(mass,px,py,pz)
+        event['muon/px'].append(px)
+        event['muon/py'].append(py)
+        event['muon/pz'].append(pz)
+        event['muon/e'].append(energy)
+        event['muon/q'].append(2*np.random.randint(2) - 1)
+
+    nelectron = np.random.randint(16)
+    event['electron/nelectron'] = nelectron
+    for n in range(nelectron):
+        px = 300*beta(2,9)
+        py = 300*beta(2,9)
+        pz = 300*beta(2,9)
+        mass = 0.000511
+        energy = calc_energy(mass,px,py,pz)
+        event['electron/px'].append(px)
+        event['electron/py'].append(py)
+        event['electron/pz'].append(pz)
+        event['electron/e'].append(energy)
+        event['electron/q'].append(2*np.random.randint(2) - 1)
+
+    nphoton = np.random.randint(16)
+    event['photon/nphoton'] = nphoton
+    for n in range(nphoton):
+        px = 300*beta(2,9)
+        py = 300*beta(2,9)
+        pz = 300*beta(2,9)
+        mass = 0.0
+        energy = calc_energy(mass,px,py,pz)
+        event['photon/px'].append(px)
+        event['photon/py'].append(py)
+        event['photon/pz'].append(pz)
+        event['photon/e'].append(energy)
+        
+    hp.fill(data,event)
+
+print("Writing the file...")
+#hdfile = write_to_file('output.hdf5',data)
+name = 'HEP_narrow_file_n%d.hdf5' % (nevents)
+hdfile = hp.write_to_file(name,data,comp_type='gzip',comp_opts=9)
+#'''
+
